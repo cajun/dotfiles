251bc3648d1f98e9e877d21da263838b48eef3db
Cajun <zac@kleinpeter.org>
2022-02-17 05:25:02 -0600

    feat: adding icons to gutter

--- nvim/init.lua
+++ nvim/init.lua
@@ -137,6 +137,12 @@ vim.o.undoreload = 10000
 vim.o.termguicolors = true
 vim.cmd([[colorscheme catppuccin ]])
 
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
 vim.cmd([[
   augroup TrimWhitespace
   autocmd!
